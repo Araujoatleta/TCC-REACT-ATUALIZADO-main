59631cdcf01f6969c390534563724ba83512ba93
Araujoatleta <eu.eduatleta@gmail.com>
2024-10-16 09:29:48 -0300

    TCC-WEB-REACT-ATUALIZADO

--- src/templates/Desktop/Desktop.jsx
+++ src/templates/Desktop/Desktop.jsx
@@ -73,6 +73,58 @@ const Desktop = () => {
 
     return (
       <>
+        <header>     
+    <aside>
+      <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          
+          <ul className="menu" data-theme='t-orange'>
+          <Link to={'/'}>
+                  <li>Home</li>
+              </Link>
+             
+              <Link to={'/home'}>
+        
+                  <li>Barber Area</li>
+                  </Link>
+              
+              <Link to={'/desktop'}>
+                  <li>Desktop</li>
+              </Link>
+
+              <Link to={'/perfil'}>
+                 <li>Editar Perfil</li>
+               </Link>
+              <Link to={'/relatorio'}>
+                <li>Relatórios</li>
+            </Link>
+            <Link to={'/agenda'}>
+                <li>Agenda</li>
+            </Link>
+          </ul>
+      </div>
+  </aside>
+    </header>
+  
+  <section className="s-hero"  style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="container">
+        <div className="left-area">
+          <h1>Admin Area</h1>
+          
+          <p> 
+            Olá! Esta é a área administrativa do nosso site, aqui você pode gerir sua(s) barbearia(s),
+            membros da sua equipe, ver avaliações e etc. 
+            Clique em nossa logo presente no canto superior esquerdo para abrir o menu de opções. 
+          </p>
+          </div>
+        </div>
+    </section>
+       
+       
+       
         <section className="carousel1" style={{ display: 'flex', overflowX: 'hidden' }} onWheel={(e) => handleWheel(e, carousel)}>
           <div className="button1s left">
             <button className='btt' onClick={() => carousel.current.scrollLeft -= carousel.current.offsetWidth}>
